004f032f92d5e6febccdd013b1448aa0e3890d2c
anuvihas <anuvihsa2005@gmail.com>
2024-09-19 21:45:55 +0530

    updated

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+// Sets EJS as the view engine and serve static files from the "public" folder
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Renders the index template with default values
+app.get("/", (req, res) => res.render("index", { weather: null, error: null }));
+
+// Handles the /weather route
+app.get("/weather", async (req, res) => {
+  const { city } = req.query;
+  const apiKey = "194a3f83b9c0aebcf36009a39afa01cb";
+  const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  
+// Checks if any error occurs during the API call
+  try {
+    const { data } = await axios.get(APIUrl);
+    res.render("index", { weather: data, error: null });
+  } catch {
+    res.render("index", { weather: null, error: "Error, Enter the correct name" });
+  }
+});
+
+// Starts the server
+const port = 3000;
+app.listen(port, () => console.log(`App is running on http://localhost:${port}`));
